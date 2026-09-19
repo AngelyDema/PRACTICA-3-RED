2529e0fe738362cdda7aeab73474c70f779627c9
AngelyDema <angelydema@gmail.com>
2026-09-18 23:00:39 -0500

    add arranque.sh

--- main.tf
+++ main.tf
@@ -23,3 +23,25 @@ resource "google_compute_subnetwork" "publica" {
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+
+resource "google_compute_instance" "app" {
+  name         = "${var.prefijo}-app"
+  machine_type = var.tipo_maquina
+  zone         = var.zona
+  tags         = ["dema"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    # la máquina debe quedar en tu subred, no en la default
+    subnetwork = google_compute_subnetwork.publica.self_link
+    # un bloque vacío aquí otorga una IP pública efímera
+    access_config {}
+  }
+
+  metadata_startup_script = file("arranque.sh")
+}
